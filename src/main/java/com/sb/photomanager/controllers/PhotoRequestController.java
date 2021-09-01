68ad420ce9e31d15c37f5a31ff7caf8b590a3c0c
Sagar Bhatia <sagar101283cse@gmail.com>
2021-08-31 23:57:06 -0400

    created post request

--- src/main/java/com/sb/photomanager/controllers/PhotoRequestController.java
+++ src/main/java/com/sb/photomanager/controllers/PhotoRequestController.java
@@ -1,14 +1,25 @@
 package com.sb.photomanager.controllers;
 
+import com.sb.photomanager.models.ImageDbRepo;
+import com.sb.photomanager.service.FileLocationService;
 import com.sb.photomanager.util.ResponseTransfer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController("/")
 public class PhotoRequestController {
+
+    @Autowired
+    private FileLocationService fileLocationService;
+    @Autowired
+    private ImageDbRepo imageDbRepo;
+
+    Logger logger = Logger.getLogger(PhotoRequestController.class.getName());
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -24,7 +35,15 @@ public class PhotoRequestController {
 
     @PostMapping(value = "/image", produces =  MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseTransfer saveImage(String userId){
+    public ResponseTransfer saveImage(@RequestParam MultipartFile imageFile){
+
+        try {
+            logger.log(Level.INFO, "Request received to save image");
+            fileLocationService.save(imageFile.getBytes(), imageFile.getName());
+        } catch (Exception e){
+            logger.log(Level.ALL, "Couldn't upload image: "+e.getMessage());
+            return new ResponseTransfer("Image couldn't be Uploaded");
+        }
         return new ResponseTransfer("Image Uploaded");
     }
 
